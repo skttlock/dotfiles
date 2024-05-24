ae2049be0b8b131057fc716b7c6acd290b4e921d
Joshua Foreman <49174473+skttlock@users.noreply.github.com>
2024-05-24 14:57:25 -0700

    make font directory

--- first_time_setup/font_installs.sh
+++ first_time_setup/font_installs.sh
@@ -7,6 +7,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 RELATIVE_FOLDER="fonts/liberation-fonts-ttf-2.1.5"
 FOLDER_PATH="$SCRIPT_DIR/$RELATIVE_FOLDER/*/"
 DESTINATION_DIR="~/.local/share/fonts/"
+mkdir $DESTINATION_DIR
 cp -r $FOLDER_PATH $DESTINATION_DIR
 fc-cache -v
 
