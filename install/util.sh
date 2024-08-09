9c4b74d718e335081c790e2f8bc9fc52664e9a55
Joshua Foreman <49174473+skttlock@users.noreply.github.com>
2024-08-09 13:06:41 -0700

    keep it simple stupid!

---
+++ install/util.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#text colors & formatting things
+# note: all colors are bolded
+RED='\e[1;31m'
+GREEN='\e[1;32m'
+BLUE='\e[1;34m'
+YELLOW='\e[1;33m'
+MAGENTA='\e[1;35m'
+WHITE='\e[1;37m'
+ONBLACK='\e[40m'
+UNDERLINE='\e[4m'
+
+STAGE=$WHITE$ONBLACK$UNDERLINE
+ERROR=$RED
+SUCCESS=$GREEN
+INFO=$BLUE
+WARNING=$YELLOW
+TIP=$MAGENTA
+RESET='\e[0m'
